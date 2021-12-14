8bb505cf7fb44af144609f10cadc698c60809684
Olle Burlin <burlinolle@gmail.com>
2021-12-14 21:58:19 +0100

    Commit

--- src/components/biljetter/Biljett.js
+++ src/components/biljetter/Biljett.js
@@ -3,15 +3,15 @@ import React from "react"
 export default function Biljett({ post }) {
   const { title, informationProgram } = post
   return (
-    <div className="flex flex-row justify-between items-start space-x-4 odd:bg-brandpurple odd:bg-opacity-75 p-4 uppercase text-pink-100">
-      <div className="flex-none text-brandpink">
-        {informationProgram.startdatum}
-      </div>
+    <div className="text-xl flex flex-row justify-between items-start space-x-4  p-4 uppercase ">
+      <div className="flex-none ">{informationProgram.startdatum}</div>
 
-      <div className="flex-1 text-gray-900 dark:text-pink-100">{title}</div>
+      <div className="flex-1 ">{title}</div>
       <div className="flex-none">
         {" "}
-        <button className="bg-brandteal py-1 px-2 text-sm">Köp</button>
+        <button className="bg-brandteal text-white py-2 px-3">
+          Köp biljett
+        </button>
       </div>
     </div>
   )
--- src/components/biljetter/Biljetter.js
+++ src/components/biljetter/Biljetter.js
@@ -19,9 +19,9 @@ export default function Biljetter() {
   `)
   const posts = data.allWpProgrampunkt.nodes
   return (
-    <div className="w-full text-pink-100">
+    <div className="w-full ">
       <div className="text-center my-4">
-        <button className="bg-brandteal text-lg px-3 py-2">
+        <button className="bg-brandteal text-lg px-3 py-2 text-white">
           <a href="https://www.tickster.com/sv/events/nm8kdtw3b485ram/2045-01-01/presentkort">
             Köp presentkort
           </a>
