aeaa5c7d066b025d32efc335e5a766f72010623c
Olle Burlin <burlinolle@gmail.com>
2021-12-14 21:28:54 +0100

    Cooomiiit

--- src/components/common/FeaturedImage.js
+++ src/components/common/FeaturedImage.js
@@ -5,13 +5,15 @@ export default function FeaturedImage({ image }) {
   const imageData = getImage(image?.node.localFile.childImageSharp)
 
   return (
-    <div className="relative z-0">
-      <div className="before:absolute before:bottom-5 before:right-6  before:z-10 before:mix-blend-multiply   before:w-full before:h-full before:bg-brandorange dark:before:bg-brandpurple before:bg-opacity-90 dark:before:bg-opacity-70 ">
-        <GatsbyImage
-          image={imageData}
-          alt={image?.node.title}
-          className="grayscale"
-        />
+    <div className="mx-8">
+      <div className="relative z-0 ">
+        <div className="before:absolute before:bottom-5 before:right-6  before:z-10 before:mix-blend-multiply   before:w-full before:h-full before:bg-brandorange dark:before:bg-brandpurple before:bg-opacity-90 dark:before:bg-opacity-70 ">
+          <GatsbyImage
+            image={imageData}
+            alt={image?.node.title}
+            className="grayscale"
+          />
+        </div>
       </div>
     </div>
   )
