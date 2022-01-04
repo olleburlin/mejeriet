6ab79cc8c355340b74026c216444c3f32060d355
Olle Burlin <burlinolle@gmail.com>
2022-01-04 20:53:32 +0100

    Commit

--- src/components/common/SinglePage.js
+++ src/components/common/SinglePage.js
@@ -3,8 +3,8 @@ import FeaturedImage from "./FeaturedImage"
 import PageHeader from "./PageHeader"
 export default function SinglePage({ data }) {
   const { content, featuredImage, kartor } = data.wpPage
-  const kartaLight = kartor.kartaLight.localFile.publicURL
-  const kartaDark = kartor.kartaDark.localFile.publicURL
+  const kartaLight = kartor?.kartaLight?.localFile.publicURL
+  const kartaDark = kartor?.kartaDark?.localFile.publicURL
   return (
     <div id="page-template" className="min-h-screen ">
       <div className="max-w-4xl mx-auto space-y-12">
