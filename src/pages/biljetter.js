af27a6053ba8dd70f3c47155315204bc26b28a2b
Olle Burlin <burlinolle@gmail.com>
2022-02-02 14:37:56 +0100

    Commit

--- src/pages/biljetter.js
+++ src/pages/biljetter.js
@@ -2,10 +2,15 @@ import React from "react"
 import Biljetter from "../components/biljetter/Biljetter"
 import PageHeader from "../components/common/PageHeader"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function BiljetterPage() {
   return (
     <Layout>
+      <SEO
+        keywords={[`Mejeriet`, `Biljetter`, `Konserter`]}
+        title="Biljetter"
+      />
       <PageHeader text="Biljetter" />
       <div className="max-w-4xl mx-auto">
         <Biljetter />
