b7a6deef924dbb58c35587687ebe58d6b418ef15
Olle Burlin <burlinolle@gmail.com>
2022-02-02 09:56:06 +0100

    Commit

--- src/components/program/ProgrampunktPage.js
+++ src/components/program/ProgrampunktPage.js
@@ -115,8 +115,12 @@ export default function ProgrampunktPage({ post }) {
             {youTubeId && (
               <div className=" embed-container">
                 <iframe
+                  width="560"
+                  height="315"
                   src={"https://www.youtube.com/embed/" + youTubeId}
+                  title="YouTube video player"
                   frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowfullscreen
                 ></iframe>
               </div>
