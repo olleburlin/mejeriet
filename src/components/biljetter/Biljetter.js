9577fc2eb1e9badbdf754ce997cd8b0f80b47ee0
Olle Burlin <burlinolle@gmail.com>
2022-02-02 14:29:20 +0100

    Commit

--- src/components/biljetter/Biljetter.js
+++ src/components/biljetter/Biljetter.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { getCurrentDate } from "../../utils/getCurrentDate"
+import Link from "../common/Link"
 export default function Biljetter() {
   const data = useStaticQuery(graphql`
     {
@@ -13,6 +14,7 @@ export default function Biljetter() {
           informationProgram {
             startdatum
             status
+            biljettlank
           }
         }
       }
@@ -41,28 +43,37 @@ export default function Biljetter() {
 
 function Biljett({ post }) {
   const { title, informationProgram } = post
+  const { status, biljettlank, startdatum } = informationProgram
   return (
     <div className="text-base flex flex-row justify-between items-center bg-white  uppercase text-black ">
       <div className="flex flex-row space-x-4  p-4 w-full">
-        <div className=" text-brandorange ">
-          {informationProgram.startdatum}
-        </div>
+        <div className=" text-brandorange ">{startdatum}</div>
         <div className="flex-1 font-heavy ">{title}</div>
         <div>
           {" "}
-          {informationProgram.status !== "Aktivt" && (
-            <span className="uppercase text-brandpink">
-              {informationProgram.status}
-            </span>
+          {status !== "Aktivt" && (
+            <span className="uppercase text-brandpink">{status}</span>
           )}{" "}
         </div>
       </div>
 
       <div className="flex-none  p-4 border-l-4 border-black border-dotted">
         {" "}
-        <button className="bg-brandpurple text-white py-1 px-2 text-sm rounded-sm">
-          Köp biljett
-        </button>
+        {biljettlank && status !== "Inställt" ? (
+          <div className="py-1">
+            <Link to={biljettlank}>
+              <button className="bg-brandpurple text-white rounded-sm px-3 py-2">
+                Köp biljett
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div className="py-1">
+            <button className="cursor-not-allowed bg-brandpurple bg-opacity-30 text-white rounded-sm px-3 py-2">
+              Köp biljett
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
