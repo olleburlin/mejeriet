0849a423dc5d7a2c71b02e5548e3a6532d894dd9
Olle Burlin <burlinolle@gmail.com>
2022-02-03 11:33:52 +0100

    Commit

--- src/components/biljetter/Biljetter.js
+++ src/components/biljetter/Biljetter.js
@@ -11,6 +11,7 @@ export default function Biljetter() {
         nodes {
           title
           id
+          uri
           informationProgram {
             startdatum
             status
@@ -42,13 +43,19 @@ export default function Biljetter() {
 }
 
 function Biljett({ post }) {
-  const { title, informationProgram } = post
+  const { title, informationProgram, uri } = post
   const { status, biljettlank, startdatum } = informationProgram
   return (
     <div className="text-sm md:text-base flex flex-row justify-between items-center bg-white  uppercase text-black ">
       <div className="flex flex-row items-baseline space-x-4  p-4 w-full">
-        <div className=" text-brandorange ">{startdatum}</div>
-        <div className="flex-1 font-heavy md:text-xl">{title}</div>
+        <div className=" ">{startdatum}</div>
+        <div className="flex-1 font-heavy md:text-xl">
+          <Link to={uri} title={title}>
+            <span className="border-b-4 md:border-b-4 border-brandorange hover:border-black dark:hover:border-white hover:text-brandorange transition-all leading-relaxed">
+              {title}
+            </span>
+          </Link>
+        </div>
         <div>
           {" "}
           {status !== "Aktivt" && (
@@ -59,7 +66,7 @@ function Biljett({ post }) {
         </div>
       </div>
 
-      <div className="  p-4 border-l-4 border-black border-dotted">
+      <div className="  px-4 py-2 border-l-4 border-black border-dotted">
         {" "}
         {biljettlank && status !== "Inställt" ? (
           <div className="py-1">
