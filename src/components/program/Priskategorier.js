1284e7fce237d219d04b43f1725793d548f61508
Olle Burlin <burlinolle@gmail.com>
2022-02-17 11:48:59 +0100

    Commit

--- src/components/program/Priskategorier.js
+++ src/components/program/Priskategorier.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Priskategorier({ prices }) {
   return (
-    <div className={`${prices.length > 1 ? "flex-col" : "flex-row"} flex`}>
-      <div className="font-heavy">Pris:&nbsp;</div>
+    <div className="flex flex-col">
+      <div className="font-heavy">Pris</div>
       {prices.map(price => {
         return <Priskategori key={price.priceDescription} price={price} />
       })}
@@ -14,9 +14,12 @@ export default function Priskategorier({ prices }) {
 function Priskategori({ price }) {
   return (
     <div className="flex flex-row space-x-2">
-      <div>{price.priceDescription},</div>
+      <div>
+        {price.priceDescription + ":"}
+        {price.price && ""}
+      </div>
 
-      <div>{price.price}&nbsp;kr</div>
+      <div>{price.price ? price.price + "kr" : "fri entré"}</div>
     </div>
   )
 }
