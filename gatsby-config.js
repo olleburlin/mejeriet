a5af1b6f2e31e68f2f3faa35d7fe63aa3d315cb8
Olle Burlin <burlinolle@gmail.com>
2022-03-31 11:53:52 +0200

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: "kulturmejeriet.se",
         // defaults to false
         enableWebVitalsTracking: true,
       },
