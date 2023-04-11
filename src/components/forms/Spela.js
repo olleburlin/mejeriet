209ca0b1a40419567de132c4b5df0b99e0512dc9
Olle Burlin <burlinolle@gmail.com>
2023-04-11 11:42:22 +0200

    Commit

--- src/components/forms/Spela.js
+++ src/components/forms/Spela.js
@@ -28,21 +28,42 @@ export default function Musikskolan() {
             </label>
             <input name="email" id="email" type="email" required />
           </div>
-
-          <div className="flex flex-row gap-4 items-center">
-            <label
-              className="order-last block relative top-1"
-              style={{ display: `block`, marginBottom: `4px` }}
-              htmlFor="vuxenkollo"
-            >
-              Vuxenkollo
-            </label>
-            <input
-              type="checkbox"
-              name="vuxenkollo"
-              id="vuxenkollo"
-              value="ja"
-            />
+          <div className="col-span-2">
+            Fyll i nedan om du är intresserad av att vara med på våra
+            Vuxenkollon som arrangeras under sommaren eller om din anmälan avser
+            terminsundervisning i ett av våra Spela För Livet-band.
+          </div>
+          <div className="flex flex-col md:flex-row gap-x-2 md:gap-4  col-span-2">
+            <div className="inline-flex gap-x-2">
+              <label
+                className="order-last block relative top-1"
+                style={{ display: `block`, marginBottom: `4px` }}
+                htmlFor="vuxenkollo"
+              >
+                Vuxenkollo
+              </label>
+              <input
+                type="checkbox"
+                name="vuxenkollo"
+                id="vuxenkollo"
+                value="ja"
+              />
+            </div>
+            <div className="inline-flex gap-x-2">
+              <label
+                className="order-last block relative top-1"
+                style={{ display: `block`, marginBottom: `4px` }}
+                htmlFor="terminsundervisning"
+              >
+                Terminsundervisning
+              </label>
+              <input
+                type="checkbox"
+                name="terminsundervisning"
+                id="terminsundervisning"
+                value="ja"
+              />
+            </div>
           </div>
         </div>
 
